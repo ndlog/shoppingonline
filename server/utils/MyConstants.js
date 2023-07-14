2b762ca3036b246697ec857889a9abbcd0275235
Nguyen Duc Long <102477140+ndlog@users.noreply.github.com>
2023-07-14 11:09:06 +0700

    add client-admin, client-customer and server

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+  DB_SERVER: "clustersgp.vlbaigw.mongodb.net",
+  DB_USER: "longkk",
+  DB_PASS: "longkk",
+  DB_DATABASE: "shoppingonline",
+  EMAIL_USER: "nongninpm123@gmail.com", // Microsoft mail service
+  EMAIL_PASS: "dmiacbwvfjfibabp",
+  JWT_SECRET: "longkk",
+  JWT_EXPIRES: "7200000", // in milliseconds
+};
+module.exports = MyConstants;
